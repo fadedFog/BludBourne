b7c3b2b5cc55192075b5b42934c113a0295f81a7
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-12-06 19:48:50 +0300

    Add start files from LibGdx

---
+++ desktop/src/ru/fadedfog/bludbourne/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.fadedfog.bludbourne.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.fadedfog.bludbourne.BludBourneGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BludBourneGame(), config);
+	}
+}
