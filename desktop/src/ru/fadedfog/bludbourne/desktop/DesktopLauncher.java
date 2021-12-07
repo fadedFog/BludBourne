0a53697feb58ef785fa84f84f5afcde1516fe951
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-12-07 20:25:40 +0300

    Update main class

--- desktop/src/ru/fadedfog/bludbourne/desktop/DesktopLauncher.java
+++ desktop/src/ru/fadedfog/bludbourne/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package ru.fadedfog.bludbourne.desktop;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.fadedfog.bludbourne.BludBourneGame;
@@ -7,6 +9,14 @@ import ru.fadedfog.bludbourne.BludBourneGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new BludBourneGame(), config);
+		config.title = "BludBourne";
+		config.useGL30 = false;
+		config.width = 800;
+		config.height = 600;
+		
+		Application app = new LwjglApplication(new BludBourneGame(), config);
+		Gdx.app = app;
+		
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 	}
 }
