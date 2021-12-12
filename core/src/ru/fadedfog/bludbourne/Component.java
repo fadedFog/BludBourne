193d3e29ecbbed232d7273b2cc7ac12e3a082ef5
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-12-12 17:44:28 +0300

    Update Entity

--- core/src/ru/fadedfog/bludbourne/Component.java
+++ core/src/ru/fadedfog/bludbourne/Component.java
@@ -2,4 +2,12 @@ package ru.fadedfog.bludbourne;
 
 public interface Component {
 
+	public static final String MESSAGE_TOKEN = "";
+	
+	public static enum MESSAGE {
+	}
+
+	void receiveMessage(String fullMessage);
+
+	void dispose();
 }
