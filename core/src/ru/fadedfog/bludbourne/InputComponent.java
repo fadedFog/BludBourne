0d9a06cb7a07bea62fba86eea8459e20b08e8e12
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-12-14 07:39:18 +0300

    Implements InputComponent

--- core/src/ru/fadedfog/bludbourne/InputComponent.java
+++ core/src/ru/fadedfog/bludbourne/InputComponent.java
@@ -1,5 +1,34 @@
 package ru.fadedfog.bludbourne;
 
-public class InputComponent implements Component {
+import java.util.HashMap;
+import java.util.Map;
 
+import com.badlogic.gdx.utils.Json;
+
+public abstract class InputComponent implements Component {
+	protected Entity.Direction currentDirection = null;
+	protected Entity.State currentState = null;
+	protected Json json;
+	protected static Map<Keys, Boolean> keys = new HashMap<Keys, Boolean>();
+	protected static Map<Mouse, Boolean> mouseButtons = new HashMap<Mouse, Boolean>();
+	
+	protected enum Keys {
+		LEFT, RIGHT, UP, DOWN, QUIT;
+	}
+	
+	protected enum Mouse {
+		SELECT, DOACTION;
+	}
+	
+	static {
+		mouseButtons.put(Mouse.SELECT, false);
+		mouseButtons.put(Mouse.DOACTION, false);
+	}
+	
+	public InputComponent() {
+		json = new Json();
+	}
+	
+	public abstract void update(Entity entity, float delta);
+ 	 
 }
