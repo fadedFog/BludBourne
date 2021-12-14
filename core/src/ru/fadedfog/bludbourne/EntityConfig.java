603343111d1ba3b16e69b32bcf57f871f44a96df
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-12-14 21:14:38 +0300

    implements receiveMessage in PGC

--- core/src/ru/fadedfog/bludbourne/EntityConfig.java
+++ core/src/ru/fadedfog/bludbourne/EntityConfig.java
@@ -1,9 +1,15 @@
 package ru.fadedfog.bludbourne;
 
+import com.badlogic.gdx.utils.Array;
+
 public class EntityConfig {
 
 	public String getEntityId() {
 		return "";
+	}
+
+	public Array<AnimationConfig> getAnimationConfig() {
+		return null;
 	}
 	
 }
--- core/src/ru/fadedfog/bludbourne/PlayerGraphicsComponent.java
+++ core/src/ru/fadedfog/bludbourne/PlayerGraphicsComponent.java
@@ -1,5 +1,75 @@
 package ru.fadedfog.bludbourne;
 
-public class PlayerGraphicsComponent {
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Json;
 
+public class PlayerGraphicsComponent extends GraphicsComponent {
+	private Json json;
+	private Vector2 currentPosition;
+	
+	public PlayerGraphicsComponent() {
+		json = new Json();
+		
+	}
+
+	@Override
+	public void update(Entity entity, MapManager mapMg, Batch batch, float delta) {
+		
+	}
+
+	@Override
+	public void receiveMessage(String fullMessage) {
+		String[] string = fullMessage.split(MESSAGE_TOKEN);
+		
+		if (string.length == 0) {
+			return;
+		}
+		
+		if (string.length == 2) {
+			if (string[0].equalsIgnoreCase
+					(MESSAGE.CURRENT_POSITION.toString())) {
+				currentPosition = json.fromJson(Vector2.class, string[1]);
+			} else if (string[0].equalsIgnoreCase
+					(MESSAGE.INIT_START_POSITION.toString())) {
+				currentPosition = json.fromJson(Vector2.class, string[1]);
+			} else if (string[0].equalsIgnoreCase
+					(MESSAGE.CURRENT_STATE.toString())) {
+				currentState = json.fromJson(Entity.State.class, string[1]);
+			} else if (string[0].equalsIgnoreCase
+					(MESSAGE.CURRENT_DIRECTION.toString())) {
+				currentDirection = json.fromJson(Entity.Direction.class, string[1]);
+			} else if (string[0].equalsIgnoreCase
+					(MESSAGE.LOAD_ANIMATIONS.toString())) {
+				EntityConfig entityConfig = json.fromJson(EntityConfig.class, string[1]);
+				Array<AnimationConfig> animationConfigs = entityConfig.getAnimationConfig();
+				
+				for (AnimationConfig animationConfig: animationConfigs) {
+					Array<String> textureNames = animationConfig.getTexturePaths();
+					Array<GridPoint2> points = animationConfig.getGridPoints();
+					Entity.AnimationType animationType = animationConfig.getAnimationType();
+					float frameDuration = animationConfig.getFrameDuraction();
+					Animation<TextureRegion> animation = null;
+					
+					if (textureNames.size == 1) {
+						animation = loadAnimation(textureNames.get(0), points, frameDuration);
+					} else if (textureNames.size == 2) {
+						animation = loadAnimation(textureNames.get(0), textureNames.get(1), points, frameDuration);
+					}
+					
+					animations.put(animationType, animation);
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+	
 }
