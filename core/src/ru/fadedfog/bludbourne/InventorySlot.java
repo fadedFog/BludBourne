c4512648dffcc1187af7b006fcf4e541b08f8666
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-12-24 07:40:01 +0300

    Create a InventorySlot

---
+++ core/src/ru/fadedfog/bludbourne/InventorySlot.java
@@ -0,0 +1,71 @@
+package ru.fadedfog.bludbourne;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.utils.Array;
+
+public class InventorySlot extends Stack {
+	private Label numItemsLabel;
+	private int numItemsVal = 0;
+	private int filterItemType;
+	
+	public InventorySlot() {
+		
+	}
+	
+	public InventorySlot(int filterItemType, Image customBackgroundDecal) {
+		
+	}
+	
+	public void decrementItemCount() {
+		
+	}
+	
+	public void incrementItemCount() {
+		
+	}
+	
+	public void add(Actor actor) {
+		
+	}
+	
+	public void add(Array<Actor> array) {
+		
+	}
+	
+	public Array<Actor> getAllInventoryItems() {
+
+	}
+	
+	public void clearAllInventoryItems() {
+		
+	}
+	
+	private void checkVisibilityOfItemCount() {
+		
+	}
+	
+	public boolean hasItem() {
+		
+	}
+	
+	public int getNumItems() {
+		
+	}
+	
+	public boolean doesAcceptItemUseType(int itemUseType) {
+		
+	}
+	
+	public InventoryItem getTopInventoryItem() {
+		
+	}
+	
+	public void swapSlots(InventorySlot inventorySlotSource,
+			InventorySlot inventorySlotTarget, InventoryItem dragActor) {
+		
+	}
+	
+}
--- core/src/ru/fadedfog/bludbourne/InventoryUI.java
+++ core/src/ru/fadedfog/bludbourne/InventoryUI.java
@@ -3,6 +3,7 @@ package ru.fadedfog.bludbourne;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 
 public class InventoryUI extends Window {
+	private InventorySlot inventorySlot;
 
 	public InventoryUI() {
 		super("inventory", Utility.INVENTORYUI_SKIN);
