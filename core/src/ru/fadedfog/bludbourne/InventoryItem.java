83059b3669b8472abc91d65cb9f832d37ca420d8
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-12-24 08:01:33 +0300

    Create a InventoryItem

---
+++ core/src/ru/fadedfog/bludbourne/InventoryItem.java
@@ -0,0 +1,64 @@
+package ru.fadedfog.bludbourne;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class InventoryItem {
+	private int itemAttributes;
+	private int itemUseType;
+	private String itemShortDescription;
+	private ItemTypeID itemTypeID;
+	
+	public InventoryItem() {
+		
+	}
+	
+	public InventoryItem(TextureRegion textureRegion, int itemAttributes, ItemTypeID itemTypeID, int itemUseType) {
+		this.itemAttributes = itemAttributes;
+		this.itemTypeID = itemTypeID;
+	}
+	
+	public InventoryItem(InventoryItem inventoryItem) {
+		
+	}
+	
+	public ItemTypeID getItemTypeID() {
+		return itemTypeID;
+	}
+	
+	public void setItemTypeID(ItemTypeID itemTypeID) {
+		
+	}
+	
+	public int getItemAttributes() {
+		return itemAttributes;
+	}
+	
+	public void setItemAttributes(int itemAttributes) {
+		this.itemAttributes = itemAttributes;
+	}
+	
+	public int getItemUseType() {
+		return itemUseType;
+	}
+	
+	public void setItemUseType(int itemUseType) {
+		this.itemUseType = itemUseType;
+	}
+
+	public String getItemShortDescription() {
+		return itemShortDescription;
+	}
+
+	public void setItemShortDescription(String itemShortDescription) {
+		this.itemShortDescription = itemShortDescription;
+	}
+	
+	public boolean isStackable() {
+		return false;
+	}
+	
+	public boolean isStackable(InventoryItem candidateInventoryItem) {
+		return false;
+	}
+	
+}
