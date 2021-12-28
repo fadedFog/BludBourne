6a05441d403033431a382a10bc57f82f0406b6ab
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-12-28 06:46:36 +0300

    Create ProfileSubject for register observers.

---
+++ core/src/ru/fadedfog/bludbourne/profile/ProfileObserver.java
@@ -0,0 +1,11 @@
+package ru.fadedfog.bludbourne.profile;
+
+public interface ProfileObserver {
+	public static enum ProfileEvent {
+		PROFILE_LOADED,
+		SAVING_PROFILE,
+		CLEAR_CURRENT_PROFILE;
+	}
+	
+	void onNotify(ProfileManager profileManager, ProfileEvent event);
+}
